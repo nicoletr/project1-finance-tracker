e8da6459bc04754d6ebd2d772fc12e6b10fd5d12
MA-FS <87353040+MA-FS@users.noreply.github.com>
2021-09-30 21:46:53 +0800

    List dropdown Javascript complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,54 @@
 fetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
   )
-    .then(function (response) {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+.then(function (response) {
+    if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+})
+.then(function (data) {
+    // DO THINGS WITH TEH DATA
+    console.log(data)
+});
+
+// Dropdown Javascript
+// Create a select box dropdown and give it the ID of list-dropdown
+let dropdown = document.getElementById('list-dropdown');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Select Your Cryptocurrency';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false';
+
+fetch(url)  
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem. Status Code: ' + 
+          response.status);  
+        return;  
       }
-      return response.json();
-    })
-    .then(function (data) {
-      // DO THINGS WITH TEH DATA
-      console.log(data)
-    });
+
+      // Examine the text in the response  
+      response.json().then(function(data) {  
+        let option;
+    
+    	for (let i = 0; i < data.length; i++) {
+          option = document.createElement('option');
+      	  option.text = data[i].name;
+      	  option.value = data[i].abbreviation;
+      	  dropdown.add(option);
+    	}    
+      });  
+    }  
+  )  
+  .catch(function(err) {  
+    console.error('Fetch Error -', err);  
+  });
+
+    
